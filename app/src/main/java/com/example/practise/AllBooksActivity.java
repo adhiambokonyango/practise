74eda12e1907c7c4d324c61af742f210523d207b
adhiambokonyango <marykonyango74@gmail.com>
2020-11-16 13:44:12 +0300

    all books

--- app/src/main/java/com/example/practise/AllBooksActivity.java
+++ app/src/main/java/com/example/practise/AllBooksActivity.java
@@ -1,17 +1,54 @@
 package com.example.practise;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class AllBooksActivity extends AppCompatActivity {
     private RecyclerView booksRecyclerView;
+    private BookRecyclerViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
+        adapter = new BookRecyclerViewAdapter(this);
 
         booksRecyclerView = findViewById(R.id.booksRecyclerView);
+        booksRecyclerView.setAdapter(adapter);
+        booksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(
+                1,
+                "Sulwe",
+                "lupita",
+                15,
+                "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1554153712i/42785750._UY400_SS400_.jpg",
+                "luo narrative",
+                "long description"));
+
+        books.add(new Book(
+                2,
+                "Amara the Brave",
+                "Matt Zhang",
+                315,
+                "https://marketplace.canva.com/EAD7WWWtKSQ/1/0/251w/canva-purple-and-red-leaves-illustration-children%27s-book-cover-hNI7HYnNVQQ.jpg",
+                "luo narrative",
+                "long description"));
+
+        books.add(new Book(
+                3,
+                "Winter Solace",
+                "Angelina McDunnel",
+                315,
+                "https://lh3.googleusercontent.com/proxy/Vv7soqhyJVvFgyjsADbAw7Uhi0K42BS_uZD6QFuLOMcOgHE57ob4eleBWGw_ZD4Ebv0Gl_b2zC9bAaY1TcWZGhTCHJWlCzxxEawGi07pgy-xEBlKtB5NLh8N9moQx5mPbFwtzI9ozg",
+                "luo narrative",
+                "long description"));
+        adapter.setBooks(books);
     }
 }
--- app/src/main/java/com/example/practise/BookRecyclerViewAdapter.java
+++ app/src/main/java/com/example/practise/BookRecyclerViewAdapter.java
@@ -1,19 +1,26 @@
 package com.example.practise;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.transition.TransitionManager;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
 public class BookRecyclerViewAdapter extends RecyclerView.Adapter<BookRecyclerViewAdapter.ViewHolder>{
+    private static final String TAG = "BookRecyclerViewAdapter";
     private ArrayList<Book> books = new ArrayList<>();
     private Context mcontext;
 
@@ -31,7 +38,32 @@ public class BookRecyclerViewAdapter extends RecyclerView.Adapter<BookRecyclerVi
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+        holder.textBookName.setText(books.get(position).getName());
+        Glide.with(mcontext)
+                .asBitmap()
+                .load(books.get(position).getImageUrl())
+                .into(holder.imgBook);
 
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mcontext, books.get(position).getName() + "selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.textAuthor.setText(books.get(position).getAuthor());
+        holder.textDescription.setText(books.get(position).getShortDesc());
+
+        if (books.get(position).isExpanded()){
+            TransitionManager.beginDelayedTransition(holder.parent);
+            holder.expandedRelativeLayout.setVisibility(View.VISIBLE);
+            holder.downArrow.setVisibility(View.GONE);
+        } else {
+            TransitionManager.beginDelayedTransition(holder.parent);
+            holder.expandedRelativeLayout.setVisibility(View.GONE);
+            holder.downArrow.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
@@ -48,12 +80,37 @@ public class BookRecyclerViewAdapter extends RecyclerView.Adapter<BookRecyclerVi
         private CardView parent;
         private ImageView imgBook;
         private TextView textBookName;
+        private ImageView downArrow, upArrow;
+        private RelativeLayout expandedRelativeLayout;
+        private TextView textAuthor, textDescription;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             parent = itemView.findViewById(R.id.parent);
             imgBook = itemView.findViewById(R.id.imgBook);
             textBookName = itemView.findViewById(R.id.textBookName);
+            downArrow = itemView.findViewById(R.id.btnDownArrow);
+            upArrow = itemView.findViewById(R.id.btnUpArrow);
+            expandedRelativeLayout = itemView.findViewById(R.id.expandedRelativeLayout);
+            textAuthor = itemView.findViewById(R.id.textAuthor);
+            textDescription = itemView.findViewById(R.id.textShortDescription);
+
+            downArrow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   Book book = books.get(getAdapterPosition());
+                   book.setExpanded(!book.isExpanded());
+                   notifyItemChanged(getAdapterPosition());
+                }
+            });
+            upArrow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Book book = books.get(getAdapterPosition());
+                    book.setExpanded(!book.isExpanded());
+                    notifyItemChanged(getAdapterPosition());
+                }
+            });
         }
     }
 }
